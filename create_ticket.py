1a8d8efdcb3c470bebc6eb9d86cfdeba86c48ba0
Elias Neves <elias-neves93@hotmail.com>
2018-07-11 18:25:32 -0300

    Realizado alterações

--- create_ticket.py
+++ create_ticket.py
@@ -10,16 +10,17 @@ from unicodedata import normalize
 print ("Integration Zabbix X Jira")
 
 address = 'http://172.16.70.34:8080' # Endereço da API Jira; eg: http://localhost:8080/rest/api/2/issue
-user = 'user' # User
-password = 'pass' # Password
+user = 'monitoracao2' # User
+password = 'Net$hoe$@BR2018' # Password
 
 
 class Ticket(object):
-    def __init__(self, priority, title, description, status):
+    def __init__(self, priority, title, description, status, ack):
         self.__priority = priority
         self.__title = title
         self.__description = description
         self.__status = status
+        self.__ack = ack
 
     @property
     def priority(self):
@@ -36,6 +37,10 @@ class Ticket(object):
     @property
     def status(self):
         return self.__status
+
+    @property
+    def ack(self):
+        return self.__ack
 
 
 def make_valid_json_create_issue(priority, title, description, status):
@@ -86,7 +91,7 @@ def make_valid_json_transition_issue():
     	"comment": [
     		{
     		"add": {
-    		"body": "Normalizado."
+    		"body": "Normalizado sem intervencao."
             }
         	}
         ]
@@ -98,9 +103,9 @@ def make_valid_json_transition_issue():
     '''
     return arquivo
 
-def get_id_issue(title):
+def get_id_issue(title, url_query):
 
-    url_query = '''http://172.16.70.34:8080/rest/api/2/search?jql=project = OTI AND issuetype = Alarme AND status in (Open, "Em Priorização", "Aguardando Atendimento", "Aguardando atendimento em segundo nível", "Aguardando atendimento em terceiro nível") AND text ~ "Trigger ID: {}"'''.format(title)
+    url_query = '''{}/rest/api/2/search?jql={} AND text ~ "Trigger ID: {}"'''.format(address, url_query, title)
 
     r = requests.get(url_query, headers={'Content-Type':'application/json'}, auth=HTTPBasicAuth(user,password))
     result = json.loads(r.text)
@@ -130,18 +135,44 @@ def create_ticket(priority, title, description, status):
     return 0
 
 
-def close_ticket(title):
+def close_ticket(title, url_query):
 
 
     arquivo = make_valid_json_transition_issue()
     title = title.split(" ")
     trigger_id = title[2]
-    issue_key = get_id_issue(trigger_id)
+    issue_key = get_id_issue(trigger_id, url_query)
 
     address_transition_issue = address+'/rest/api/2/issue/{}/transitions'.format(issue_key)
 
     r = requests.post(address_transition_issue, data=arquivo, headers={'Content-Type':'application/json'}, auth=HTTPBasicAuth(user,password))
-    return 'Status Code: {} - Chamado Cancelado: {}'.format(r.status_code, r.text)
+    return 'Status Code: {} - Chamado Finalizado: {}'.format(r.status_code, r.text)
+
+def relapse(issue_key):
+
+    json_update = '''
+    {
+      "fields": {
+
+        "customfield_15902": 0
+      }
+    }'''
+
+    r = requests.get(address+'/rest/api/2/issue/{}'.format(issue_key), headers={'Content-Type':'application/json'}, auth=HTTPBasicAuth(user,password))
+
+    issue = json.loads(r.text)
+
+    relapse = issue["fields"]["customfield_15902"]
+
+    jsonObjectInfo = json.loads(json_update)
+    jsonObjectInfo["fields"]["customfield_15902"] = relapse + 1
+
+    json_final = json.dumps(jsonObjectInfo, ensure_ascii=False)
+
+    u = requests.put(address+'/rest/api/2/issue/{}'.format(issue_key),data=(json_final),headers={'Content-Type':'application/json'}, auth=HTTPBasicAuth(user,password))
+    print('Relapse {} to {}'.format(relapse, relapse+1))
+    return True
+
 
 def main():
 
@@ -151,6 +182,7 @@ def main():
     parser.add_argument("--title", metavar="Str", help="Title Issue", type=str, required=True)
     parser.add_argument("--description", metavar="Str", help="Description Issue", type=str, required=True)
     parser.add_argument("--status", metavar="Str", help="Status", type=str, required=True)
+    parser.add_argument("--ack", metavar="Str", help="Ack", type=str, required=False)
 
     args = parser.parse_args()
 
@@ -159,21 +191,30 @@ def main():
     title = args.title
     description = args.description
     status = args.status
+    ack = args.ack
 
     # Criando o OBJ do Ticket
-    ticket = Ticket(priority, title, description, status)
+    ticket = Ticket(priority, title, description, status, ack)
 
     if ticket.status == 'PROBLEM':
 
         title = ticket.title.split(" ")
         trigger_id = title[2]
+
         try:
-            issue_key = get_id_issue(trigger_id)
-        except:
+            # Tenta realizar o Relapse
+            issue_key = get_id_issue(trigger_id, url_query='project = OTI AND issuetype = Alarme AND status in (Open, "Em Execução", "Em Priorização", "Aguardando Atendimento", "Atendimento Pausado", "Aguardando Execução de Script", "Validar e Executar Script", "Aguardando Execução e Validação Manual", "Aguardando Informações", "Em Priorização do PO", "Aguardando atendimento em segundo nível", "Aguardando atendimento em terceiro nível")')
+            relapse(issue_key)
+        except Exception as e:
+            # Se não achar o chamado cria um novo
+            print ('Erro: {}'.format(e))
             create_ticket(ticket.priority, ticket.title, ticket.description, ticket.status)
 
+    elif ticket.status == 'OK':
+        print('Fechando chamados pendentes...', close_ticket(ticket.title, url_query='project = OTI AND issuetype = Alarme AND status in (Open, "Em Priorização")  AND "Indicar o time" = "Operações - NOC" AND "Nível de Atendimento" = NOC'))
+
     else:
-        print(close_ticket(ticket.title))
+        print(close_ticket(ticket.title, url_query='project = OTI AND issuetype = Alarme AND status in (Open, "Em Priorização", "Aguardando Atendimento", "Em Priorização do PO", "Aguardando atendimento em segundo nível", "Aguardando atendimento em terceiro nível")'))
 
 if __name__ == "__main__":
     main()
